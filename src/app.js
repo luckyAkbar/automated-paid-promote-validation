5ad02e23b69d1ea8a1ea03bb73db3179c5a811d9
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-12 08:46:36 +0700

    add new middleware: helmet, cookie-parser, incorrectJSONFormatHandler

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const { incorrectJSONFormatHandler } = require('./middleware/incorrectJSONFormatHandler');
 const { router } = require('./routes/router');
 
+
+const app = express();
+app.use(helmet());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ type: 'application/json' }), incorrectJSONFormatHandler);
 app.use('/', router);
 
 module.exports = { app };
