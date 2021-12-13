cb5a1554c8a5b2bd3593637fab2837fbd704bb74
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-14 06:43:17 +0700

    add new assertion to assert password and confirmation password is the same. and add new renderer for signup page

--- src/handler/signupHandler.js
+++ src/handler/signupHandler.js
@@ -1,19 +1,26 @@
 'use strict';
 
+const assert = require('assert').strict;
+const CustomError = require('../classes/CustomError');
 const Signup = require('../classes/Signup');
 
 const signupHandler = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
 
   try {
+    assert.strictEqual(password, confirmPassword, new CustomError('Password did not match'));
+
     const signup = new Signup(email, password, 'user');
     await signup.createUser();
 
     res.status(201).json({ message: `User created. You can login with your new account now.` });
   } catch (e) {
-    console.log(e);
     res.status(e.HTTPErrorStatus).json({ message: e.message });
   }
 };
 
-module.exports = { signupHandler };
+const renderSignupPage = (req, res) => {
+  res.status(200).render('sign-up-page');
+};
+
+module.exports = { signupHandler, renderSignupPage };
