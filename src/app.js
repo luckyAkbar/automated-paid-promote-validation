96f89f1e7c332496c00a05aa5c8ac4a252c68803
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-15 13:43:17 +0700

    removing helmet middleware

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ const { router } = require('./routes/router');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(helmet());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ type: 'application/json' }), incorrectJSONFormatHandler);
